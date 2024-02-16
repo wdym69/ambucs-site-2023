246111da6623d99a4950f952c6375db58536bea7
gsantella <gsantella@southhills.edu>
2024-02-16 16:19:54 -0500

    V1.2 hotfix #2 add blood drive (#4) to become 1.2.1
    * added blood screen info in modal
    
    * fix typo

---
+++ _static/modal.js
@@ -0,0 +1,20 @@
+//https://reintech.io/blog/creating-responsive-modal-window-css
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
